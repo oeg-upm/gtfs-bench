cc1199acc9cbb53f4c568b042c7d302b6fb427d6
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-08-08 16:23:10 +0200

    add service_dates and trip xr2rml mappings

--- generation/gtfs-to-mongo.sh
+++ generation/gtfs-to-mongo.sh
@@ -3,3 +3,4 @@ mongoimport --db gtfs1 --collection STOPS --file STOPS.json --jsonArray
 mongoimport --db gtfs1 --collection ROUTES --file ROUTES.json --jsonArray
 mongoimport --db gtfs1 --collection CALENDAR --file CALENDAR.json --jsonArray
 mongoimport --db gtfs1 --collection CALENDAR_DATES --file CALENDAR_DATES.json --jsonArray
+mongoimport --db gtfs1 --collection TRIPS --file TRIPS.json --jsonArray
