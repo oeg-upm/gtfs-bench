a0659d2034ed621b067ec0e320375ca2f5361214
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-08-08 14:20:00 +0200

    add route xr2rml mappings

---
+++ generation/gtfs-to-mongo.sh
@@ -0,0 +1,3 @@
+mongoimport --db gtfs1 --collection AGENCY --file AGENCY.json --jsonArray
+mongoimport --db gtfs1 --collection STOPS --file STOPS.json --jsonArray
+mongoimport --db gtfs1 --collection ROUTES --file ROUTES.json --jsonArray
