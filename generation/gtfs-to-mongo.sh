28fd5b12c14fb846ef0744fb4b3008303c331057
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-08-08 14:57:26 +0200

    add services xr2rml mappings

--- generation/gtfs-to-mongo.sh
+++ generation/gtfs-to-mongo.sh
@@ -1,3 +1,4 @@
 mongoimport --db gtfs1 --collection AGENCY --file AGENCY.json --jsonArray
 mongoimport --db gtfs1 --collection STOPS --file STOPS.json --jsonArray
 mongoimport --db gtfs1 --collection ROUTES --file ROUTES.json --jsonArray
+mongoimport --db gtfs1 --collection CALENDAR --file CALENDAR.json --jsonArray
