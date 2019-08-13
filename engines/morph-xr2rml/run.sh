0ea7b62364aebe0d09b66f1f15188379420e9e67
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-08-13 16:40:29 +0200

    update run.sh for morph-xr2rml

--- engines/morph-xr2rml/run.sh
+++ engines/morph-xr2rml/run.sh
@@ -2,33 +2,37 @@
 
 FILES=../../queries/original/*.rq
 
-echo "query, tiempo (date +%s.%N)">results/times-1.csv
-for file in $FILES
-do
-for i in 1 2 3 4 5
-do
-query=$(echo  $file | cut -d "/" -f3)
+echo "size, query, tiempo (date +%s.%N)">results/times-1.csv
+for size in 1 5 do
+    for file in $FILES do
+        #la configuracion depende de el tama–o y query
+        echo "query.file.path=$query">>morph.properties
+        echo "output.file.path=../../results/result-gtfs1-$query.xml">>morph.properties
+        echo "database.name[0]=gtfs$size">>morph.properties
 
-echo "**********************************************"
-echo $query
-echo "**********************************************"
-#sleep 5  # Waits 5 seconds.
+        for i in 1 2 3 4 5 do
+        query=$(echo  $file | cut -d "/" -f3)
 
-#a–ade la query a la configuracion
-echo "query.file.path=$query">>morph.properties
-#echo "query.file.path=$query">>data/original/morph.properties
-echo "output.file.path=../../results/result-gtfs1-$query.xml">>morph.properties
-#tiempo inicio
-start=$(date +%s)
-java -jar morph-xr2rml.jar  --configDir  data/original/
-fin=$(date +%s)
-#tiempo fin
-# resta de tiempos
-dur=$(echo "$fin - $start" | bc)
-#guardamos el tiempo
-echo "$query, $i, $dur">>results/times-1.csv
+        echo "**********************************************"
+        echo $query
+        echo "**********************************************"
+        #sleep 5  # Waits 5 seconds.
 
-#Elimina la ultima linea del fichero
-sed -i '74,75d' morph.properties
+
+        #tiempo inicio
+        start=$(date +%s)
+        java -jar morph-xr2rml.jar  --configDir  data/original/
+        fin=$(date +%s)
+        #tiempo fin
+        # resta de tiempos
+        dur=$(echo "$fin - $start" | bc)
+        #guardamos el tiempo
+        echo "$size, $query, $i, $dur">>results/times-1.csv
+
+        #Elimina la ultima linea del fichero
+        #sed -i '74,76d' morph.properties
+        done
+        head -n -3 morph.properties > temp.txt ; mv temp.txt morph.properties
+    done
 done
-done
+
