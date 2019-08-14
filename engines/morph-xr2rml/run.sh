47789ae0abaed46f7522957feb32f9240be7f7cd
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-08-14 12:07:54 +0200

    update run.sh for morph-xr2rml

--- engines/morph-xr2rml/run.sh
+++ engines/morph-xr2rml/run.sh
@@ -3,10 +3,10 @@
 FILES=queries/original/*.rq
 echo "size, query, run, time (date +%s.%N)">results/times.csv
 for size in 1 5
-    do
+do
     properties=properties/gtfs-$size.properties
     for file in $FILES
-        do
+    do
         echo "**********************************************"
 
         #la configuracion depende del tamano y query
@@ -16,29 +16,28 @@ for size in 1 5
 
         for i in 1 2 3 4 5
         do
-        if [ $size -eq 1 ]
-        then
-            query=original/$(basename $file)
-        else
-            query=vig/$(basename $file)
-        fi
+            if [ $size -eq 1 ]
+            then
+                query=original/$(basename $file)
+            else
+                query=vig/$(basename $file)
+            fi
 
-        #sleep 5  # Waits 5 seconds.
+            #sleep 2
 
+            #tiempo inicio
+            start=$(date +%s.%N)
+            echo "Evaluating: size $size - query $query - run $i"
+            #java -jar morph-xr2rml-4.0.0.jar  --configDir  data/original/
+            fin=$(date +%s.%N)
+            #tiempo fin
+            # resta de tiempos
+            dur=$(echo "$fin - $start" | bc)
+            #guardamos el tiempo
+            echo "$size, $query, $i, $dur">>results/times.csv
 
-        #tiempo inicio
-        start=$(date +%s.%N)
-        echo "Evaluating: size $size - query $query - run $i"
-        #java -jar morph-xr2rml-4.0.0.jar  --configDir  data/original/
-        fin=$(date +%s.%N)
-        #tiempo fin
-        # resta de tiempos
-        dur=$(echo "$fin - $start" | bc)
-        #guardamos el tiempo
-        echo "$size, $query, $i, $dur">>results/times.csv
-
-        #Elimina la ultima linea del fichero
         done
+        #Elimina la ultima linea del fichero
         tail -n 3 $properties | wc -c | xargs -I {} truncate $properties -s -{}
     done
 done
