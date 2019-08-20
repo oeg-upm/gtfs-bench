e63768146ab0a1b0a16a97096d50603b3b452cd0
dachafra <dachafra@gmail.com>
2019-08-21 00:18:21 +0200

    data-preparation

--- data/data-preparation.sh
+++ data/data-preparation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-declare -a arr=("csv" "json" "sql" "xml" "dist")
+declare -a arr=("csv" "json" "rdb" "xml" "dist")
 count=0
 
+# download from data-url
 while IFS=, read -r format gtfs gtfs5 gtfs10 gtfs50 gtfs100 gtfs500 gtfs1000 gtfs5000
 do
 	if [[ $gtfs =~ ^http.* ]]; then
@@ -18,10 +19,11 @@ do
 	    	break
 	    fi
 
-	     ((count++))
+	    ((count++))
 	fi
 done < data-url.csv
 
+#unzip de files
 for j in "${arr[@]}"
 do
 	for i in 1 5 10 50 100 500 1000 5000
@@ -29,3 +31,21 @@ do
 		unzip $j/gtfs-$j-$i.zip -d gtfs-$j-$i
 	done
 done
+
+rm *.zip
+
+#create the docker images from mysql (naive and ontop)
+docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose-ontop.yml up -d
+
+#copy the schema and scripts to the corresponding sql and run the load scripts
+for i in 1 5 10 50 100 500
+do
+	cp schema.sql gtfs-sql-$i/
+	cp schema-ontop.sql gtfs-sql-$i/
+	cd gtfs-sql-$i/
+	docker exec -it mysql_gtfs$1 mysql -u root -poeg gtfs -e "source schema.sql"
+	docker exec -it mysql_ontop_gtfs$1l mysql -u root -poeg gtfs -e "source schema-ontop.sql"
+done
+
+#preparation of mongodb
