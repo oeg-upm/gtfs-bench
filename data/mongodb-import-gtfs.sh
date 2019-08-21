f8c8386736f80487ab15b25bbf4db11c35c54d6e
Jhon Anibal <jhontb7@gmail.com>
2019-08-21 15:30:35 +0200

    Update mongodb-import-gtfs.sh

--- data/mongodb-import-gtfs.sh
+++ data/mongodb-import-gtfs.sh
@@ -1,9 +1,13 @@
-mongoimport --db gtfs1 --collection AGENCY --file AGENCY.json --jsonArray
-mongoimport --db gtfs1 --collection STOPS --file STOPS.json --jsonArray
-mongoimport --db gtfs1 --collection ROUTES --file ROUTES.json --jsonArray
-mongoimport --db gtfs1 --collection CALENDAR --file CALENDAR.json --jsonArray
-mongoimport --db gtfs1 --collection CALENDAR_DATES --file CALENDAR_DATES.json --jsonArray
-mongoimport --db gtfs1 --collection TRIPS --file TRIPS.json --jsonArray
-mongoimport --db gtfs1 --collection STOP_TIMES --file STOP_TIMES.json --jsonArray
+use gtfs
+mongoimport --db gtfs --collection AGENCY --file AGENCY.json --jsonArray
+mongoimport --db gtfs --collection STOPS --file STOPS.json --jsonArray
+mongoimport --db gtfs --collection ROUTES --file ROUTES.json --jsonArray
+mongoimport --db gtfs --collection CALENDAR --file CALENDAR.json --jsonArray
+mongoimport --db gtfs --collection CALENDAR_DATES --file CALENDAR_DATES.json --jsonArray
+mongoimport --db gtfs --collection TRIPS --file TRIPS.json --jsonArray
+mongoimport --db gtfs --collection STOP_TIMES --file STOP_TIMES.json --jsonArray
+mongoimport --db gtfs --collection FEED_INFO --file FEED_INFO.json --jsonArray
+mongoimport --db gtfs --collection FREQUENCIES --file FREQUENCIES.json --jsonArray
+mongoimport --db gtfs --collection SHAPES --file SHAPES.json --jsonArray
 
 mongo < mongodb-set-null-values.js
