cbf63c676cfa8f894ac025348d011d7ca11031f7
Jhon Anibal <jhontb7@gmail.com>
2019-08-22 16:22:19 +0200

    new test

--- data/data-preparation.sh
+++ data/data-preparation.sh
@@ -3,15 +3,15 @@ declare -a arr=("csv" "json" "rdb" "xml" "dist")
 count=0
 
 # download from data-url
-while IFS=, read -r format gtfs gtfs5 gtfs10 gtfs50 #gtfs100 gtfs500 gtfs1000 gtfs5000
+while IFS=, read -r format gtfs gtfs5 gtfs10 gtfs50 gtfs100 gtfs500 #gtfs1000 gtfs5000
 do
 	if [[ $gtfs =~ ^http.* ]]; then
 	    wget -O gtfs-${arr[$count]}-1.zip $gtfs 
 	    wget -O gtfs-${arr[$count]}-5.zip $gtfs5 
 	    wget -O gtfs-${arr[$count]}-10.zip $gtfs10 
 	    wget -O gtfs-${arr[$count]}-50.zip $gtfs50 
-	    #wget -O gtfs-${arr[$count]}-100.zip $gtfs100 
-	    #wget -O gtfs-${arr[$count]}-500.zip $gtfs500 
+	    wget -O gtfs-${arr[$count]}-100.zip $gtfs100 
+	    wget -O gtfs-${arr[$count]}-500.zip $gtfs500 
 	    #wget -O gtfs-${arr[$count]}-1000.zip $gtfs1000 
 	    #wget -O gtfs-${arr[$count]}-5000.zip $gtfs5000
 
@@ -27,7 +27,7 @@ done < data-url.csv
 #unzip de files
 for j in "${arr[@]}"
 do
-	for i in 1 5 10 50 #100 500 #1000 5000
+	for i in 1 5 10 50 100 500 #1000 5000
 	do
 		unzip gtfs-$j-$i.zip -d gtfs-$j-$i
 	done
@@ -40,20 +40,20 @@ docker-compose -f docker-compose.yml up -d
 docker-compose -f docker-compose-ontop.yml up -d
 
 #copy the schema and scripts to the corresponding sql and run the load scripts
-for i in 1 5 10 50 #100 500
+for i in 1 5 10 50 100 500
 do
 	cp schema.sql gtfs-rdb-$i/
 	cp schema-ontop.sql gtfs-rdb-$i/
 
 	docker exec -it -w /data/  gtfs${i}_mysql mysql -u root -poeg  -e 'source schema.sql'
-	docker exec -it -w /data/   gtfs${i}_ontop_mysql mysql -u root -poeg  -e 'source schema-ontop.sql'
+#	docker exec -it -w /data/   gtfs${i}_ontop_mysql mysql -u root -poeg  -e 'source schema-ontop.sql'
 #	docker exec -it -w /data/  gtfs$i_mysql mysql -u root -poeg  -e 'source schema.sql'
 #	docker exec -it -w /data/  gtfs$i_ontop_mysql mysql -u root -poeg  -e 'source schema-ontop.sql'
 
 done
 
 #preparation of mongodb
-for i in 1 5 10 50 #100 500
+for i in 1 5 10 50 100 500
 do
 	cp mongodb-* gtfs-json-$i/
 	docker exec -it -w /data gtfs${i}_mongo ./mongodb-import-gtfs.sh
