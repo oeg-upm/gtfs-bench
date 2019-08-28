a5219cd9860f323f50bf63d7c931ea2ef5f4fb11
Jhon Anibal <jhontb7@gmail.com>
2019-08-28 15:29:53 +0200

    Update evaluate_morph-xr2rml_cold.sh

--- engines/morph-xr2rml/evaluate_morph-xr2rml_cold.sh
+++ engines/morph-xr2rml/evaluate_morph-xr2rml_cold.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sh ../evaluate.sh ../../queries/original/ gtfs.morph-xr2rml.properties morph-xr2rml 0
-
+sh evaluate.sh ../queries/original/ gtfs.morph-xr2rml.properties  morph-xr2rml 0
