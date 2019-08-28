c0d44a6793422e2330a88e1e990f2ae4a02f2ebe
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-08-28 16:08:17 +0200

    update script restart mongo

--- engines/morph-xr2rml/restart_database_morph-xr2rml.sh
+++ engines/morph-xr2rml/restart_database_morph-xr2rml.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-#sudo service mongod restart
+size=$1
+
 echo "restarting database ..."
+
+echo "executing docker stop gtfs${size}__mongo ..."
+docker stop gtfs${size}_mongo
+echo "executing docker start gtfs${size}__mongo ..."
+docker start gtfs${size}__mongo
+echo "tired after restaring docker, going to sleep for 60 seconds ..."
+sleep 60
+echo "waking up from sleep, feeling so fresh"
+
