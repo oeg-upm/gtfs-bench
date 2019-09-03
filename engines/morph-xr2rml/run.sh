584476bba804692968a752c561b7abbaa9d60c4c
Jhon Anibal <jhontb7@gmail.com>
2019-09-03 15:33:28 +0200

    Update run.sh

--- engines/morph-xr2rml/run.sh
+++ engines/morph-xr2rml/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-size=$1
-query=$2
-run=$3
+
 echo "size, query, run, time (date +%s.%N)">../results/results-times.csv
 #start time
-start=$(date +%s.%N)
-#java -jar morph-xr2rml-4.0.0.jar  --configDir .  --configFile gtfs.morph-xr2rml.properties
-#finish time
-finish=$(date +%s.%N)
-#duration
-dur=$(echo "$finish - $start" | bc)
-
-echo "$size, $query, $run, $dur">>../results/results-times.csv
+for size in 1 5 
+do
+    for query in {1..17}
+    do
+        #add propertie file config
+        
+        for run in 1 2 3 4 5 6
+        do
+            start=$(date +%s.%N)
+            java -jar morph-xr2rml-4.0.0.jar  --configDir .  --configFile gtfs.morph-xr2rml.properties
+            #finish time
+            finish=$(date +%s.%N)
+            #duration
+            dur=$(echo "$finish - $start" | bc)
+            echo "$size, $query, $run, $dur">>../results/results-times.csv
+        done 
+    done
+done
