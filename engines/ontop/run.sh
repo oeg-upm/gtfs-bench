a271bff7e2980ce1f27530a53d28fda516a37ee0
dachafra <dachafra@gmail.com>
2019-09-04 17:01:51 +0200

    ontop scripts

---
+++ engines/ontop/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#properties, mapping, querypath, size,query,time
+size=$4
+query=$5
+run=$6
+#echo "size, query, run, time (date +%s.%N)">../results/results-times.csv
+#start time
+start=$(date +%s.%N)
+./ontop query -m $2 -p $1 -q $3
+#finish time
+finish=$(date +%s.%N)
+#duration
+dur=$(echo "$finish - $start" | bc)
+
+echo "$size, $query, $run, $dur">>../results/results-times.csv
+
