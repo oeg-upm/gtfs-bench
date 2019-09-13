e762d03313f9a2b7e3efd650039480da1acfd984
Jhon Anibal <jhontb7@gmail.com>
2019-09-14 00:06:17 +0200

    clean VIG data

--- generation/run.sh
+++ generation/run.sh
@@ -5,6 +5,7 @@ for i in 5 10 50 100 500 1000 5000
 do
  java -jar vig-distribution-1.8.0-jar-with-dependencies.jar --res=resources --scale=$i
  cd src/main/resources/csvs
+ ./clean.sh
  ./headers.sh
  zip $i.zip *.csv
  rm *.csv
