63565c1c5a6b3df303a14001519cc812dd6462c4
Jhon Anibal <jhontb7@gmail.com>
2019-09-16 10:00:48 +0200

    Update clean.sh

--- generation/src/main/resources/csvs/clean.sh
+++ generation/src/main/resources/csvs/clean.sh
@@ -23,7 +23,7 @@ cat tmp.txt > CALENDAR.csv
 
 #ROUTES
 
-awk -F "," '{print $1","$2-1","$3","$4","$5","$6-1","$7","$8"," $9"," $10}' ROUTES.csv> tmp.txt
+awk -F "," '{print $1","$2-1","$3","$4","$5","$6-1","$7","$8"," $9}' ROUTES.csv> tmp.txt
 cat tmp.txt> ROUTES.csv
 
 #STOP_TIMES
