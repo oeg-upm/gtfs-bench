ae01c8306232067f33fdd67204780be023630c5e
Jhon Anibal <jhontb7@gmail.com>
2019-09-19 10:37:18 +0200

    Update evaluate_warm.sh

--- engines/morph-rdb/evaluate_warm.sh
+++ engines/morph-rdb/evaluate_warm.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-#sh ../evaluate.sh ../../queries properties/gtfs.morph-rdb.properties morph-rdb 1 ../../results
+
+for i in 1 5 10 50 100 500
+do
+	for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+	do
+		for t in 1 2 3 4 5
+		do
+			#properties, mapping, querypath, size,query,time
+			if [[ $i -eq 1 ]];then
+				# Load properties configuration
+                ./pre_update_config.sh gtfs.morph-rdb.properties $i q${j}.rq
+                # Run engine
+                ./run.sh $i q${j}.rq $t 'warm'
+                # Delete properties configuration
+                ./post_update_config.sh gtfs.morph-rdb.properties
+			else
+				#VIG queries
+                # Load properties configuration
+                ./pre_update_config.sh gtfs.morph-rdb.properties $i q${j}.rq
+                # Run engine
+                ./run.sh $i q${j}.rq $t 'warm'
+                # Delete properties configuration
+                ./post_update_config.sh gtfs.morph-rdb.properties
+			fi
+		done
+	done
+done
