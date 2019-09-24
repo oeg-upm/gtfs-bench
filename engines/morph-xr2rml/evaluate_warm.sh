edf99541ca55d6fa9f7e7776830c62925550f13c
Jhon Anibal <jhontb7@gmail.com>
2019-09-24 19:52:26 +0200

    Update evaluate_warm.sh

--- engines/morph-xr2rml/evaluate_warm.sh
+++ engines/morph-xr2rml/evaluate_warm.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-sh ../evaluate.sh ../../queries/original/ gtfs.morph-xr2rml.properties morph-xr2rml 1 result
 
+for i in 1 5 10 50 100 500
+do
+	for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+	do
+		for t in 1 2 3 4 5
+		do
+			#properties, mapping, querypath, size,query,time
+			if [ $i -eq 1 ]
+			then
+				# Load properties configuration
+                ./pre_update_config.sh gtfs.morph-xr2rml.properties $i q${j}.rq
+                # Run engine
+                ./run.sh $i q${j}.rq $t 'warm'
+                # Delete properties configuration
+                ./post_update_config.sh gtfs.morph-xr2rml.properties
+			else
+				#VIG queries
+                # Load properties configuration
+                ./pre_update_config.sh gtfs.morph-xr2rml.properties $i q${j}.rq
+                # Run engine
+                ./run.sh $i q${j}.rq $t 'warm'
+                # Delete properties configuration
+                ./post_update_config.sh gtfs.morph-xr2rml.properties
+			fi
+		done
+	done
+done
