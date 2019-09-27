7d5cb1a9a19292b674828eb094da3ece6d53b739
dachafra <dachafra@gmail.com>
2019-09-27 18:06:05 +0200

    ontario configs

---
+++ engines/ontario/evaluate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+declare -a array=("csv" "json" "xml" "best" "worst" "random")
+
+echo "size, query, run, type,time (date +%s.%N)" > ../results/results-times.csv
+
+for p in "${array[@]}"
+do
+	for i in 1 5 10 50 100 500
+	do
+		if [ $i -eq 1 ]
+		then
+			path=/queries/original
+		else
+			path=/queries/vig
+		fi
+		cp /data/gtfs-$p-$i/* /data/
+		/Ontario/scripts/create_rdfmts.py -s /configurations/datasources-$p.json -o /configurations/myconfig.json
+		for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+		do
+			for t in 1 2 3 4 5
+			do
+				echo "size $i query $j run $t $p"
+				timeout -s SIGKILL 60m  ./run.sh $i $path/q${j}.rq $t $p ||echo "$i, q${j}.rq, $t, $p, TimeOut">> ../results/results-times.csv                                       
+			done
+		done
+		rm /data/*.*
+		if [ $p -eq "worst" ]
+		then
+			sed -i 's/worst/random/g' /configurations/datasources-dist.json
+		fi
+		if [ $p -eq "best" ]
+		then
+			sed -i 's/best/worst/g' /configurations/datasources-dist.json
+		fi
+	done
+
+done
+
+rm /data/*.*
