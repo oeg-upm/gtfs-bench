fb1240173c93f9601454e1b8197e9f2b9d704a70
Jhon Anibal <jhontb7@gmail.com>
2019-09-30 14:17:01 +0200

    Update evaluate_warm.sh

--- engines/morph-xr2rml/evaluate_warm.sh
+++ engines/morph-xr2rml/evaluate_warm.sh
@@ -6,15 +6,13 @@ do
 	do
 		for t in 1 2 3 4 5
 		do
-
 			# Load properties configuration
-			echo  "size-$i-q${j}.rq-run-$t-'warm'"
 			./pre_update_config.sh gtfs.morph-xr2rml.properties $i q${j}.rq 'warm'
 			# Run engine
 			timeout -s SIGKILL 60m  ./run.sh $i q${j}.rq $t 'warm' ||echo "$i, q${j}.rq, $t, warm, TimeOut">> ../results/results-times.csv
 			# Delete properties configuration
 			./post_update_config.sh gtfs.morph-xr2rml.properties
-			
+
 		done
 	done
 done
