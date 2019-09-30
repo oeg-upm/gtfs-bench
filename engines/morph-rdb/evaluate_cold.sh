e103ae4304b7a48dc1207ffefbdbebf29b686e19
Jhon Anibal <jhontb7@gmail.com>
2019-09-30 10:14:33 +0200

    updated  test morph-rdb

--- engines/morph-rdb/evaluate_cold.sh
+++ engines/morph-rdb/evaluate_cold.sh
@@ -8,39 +8,26 @@ echo "size, query, run, type,time (date +%s.%N)" > ../results/results-times.csv
 
 for i in 1 5 10 50 100 500
 do
-        for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
-        do
-                for t in 1 2 3 4 5
-                do
-					#properties, mapping, querypath, size,query,time
-					if [ $i -eq 1 ]
-					then
-									echo "size $i  query $j  run $t"
-									# original queries
-									#./run.sh $size, $query, $run, $type
-									#echo  $i q${j}.rq $t 'cold'
-									# Load properties configuration
-									./pre_update_config.sh gtfs.morph-rdb.properties $i q${j}.rq
-									# Run engine ----> add timeout 60min
+	for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+	do
+		for t in 1 2 3 4 5
+		do
+			#properties, mapping, querypath, size,query,time
+			echo "size $i  query $j  run $t"
+			#echo  $i q${j}.rq $t 'cold'
+			# Load properties configuration
+			./pre_update_config.sh gtfs.morph-rdb.properties $i q${j}.rq 'cold'
+			# Run engine ----> add timeout 60min
+			timeout -s SIGKILL 60m  ./run.sh $i q${j}.rq $t 'cold' ||echo "$i, q${j}.rq, $t, cold, TimeOut">> ../results/results-times.csv
+			# Delete properties configuration
+			./post_update_config.sh gtfs.morph-rdb.properties
 
-									timeout -s SIGKILL 60m  ./run.sh $i q${j}.rq $t 'cold' ||echo "$i, q${j}.rq, $t, cold, TimeOut">> ../results/results-times.csv
-									# Delete properties configuration
-									./post_update_config.sh gtfs.morph-rdb.properties
-					else
-									#VIG queries
-									# Load properties configuration
-									./pre_update_config.sh gtfs.morph-rdb.properties $i q${j}.rq
-									# Run engine
-									./run.sh $i q${j}.rq $t 'cold'
-									# Delete properties configuration
-									./post_update_config.sh gtfs.morph-rdb.properties
-					fi
-					# restart database
-					echo "delete :  /data/gtfs-rdb-$i/flag.txt"
-					echo "Restart data base..."
-					rm /data/gtfs-rdb-$i/flag.txt
-					sleep 2m
+			# restart database
+			echo "delete :  /data/gtfs-rdb-$i/flag.txt"
+			echo "Restart data base..."
+			rm /data/gtfs-rdb-$i/flag.txt
+			sleep 2m
 
-                done
-        done
+		done
+	done
 done
