79b98c024b0837acaea93384a76f9856b9e0e85b
Jhon Anibal <jhontb7@gmail.com>
2019-09-30 15:01:07 +0200

    Update run.sh

--- engines/ontario/run.sh
+++ engines/ontario/run.sh
@@ -4,7 +4,7 @@ size=$1
 query=$2
 run=$3
 typ=$4
-
+flag=0
 if [ $size -eq 1 ]
     then
         path=/queries/original/$query
@@ -13,10 +13,17 @@ if [ $size -eq 1 ]
 fi
 #start time
 start=$(date +%s.%N)
-/Ontario/scripts/runExperiment.py -c /configurations/myconfig.json -q $path -r True
+/Ontario/scripts/runExperiment.py -c /configurations/myconfig.json -q $path -r True > ../results/result-gtfs${size}-$query.log || flag=1
 #finish time
 finish=$(date +%s.%N)
 #duration
 dur=$(echo "$finish - $start" | bc)
+
+if [ $flag -eq 1 ]
+then
+        dur=Error
+fi
+
 #echo "$size, $query, $run, $type, $dur">>../results/results-times.csv
 echo "$size, $query, $run, $typ, $dur">>../results/results-times.csv
+
