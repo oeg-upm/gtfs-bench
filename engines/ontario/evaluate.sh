15eed07fbc516d92cbd8ebfe64ca789f5f9d1c7d
Jhon Anibal <jhontb7@gmail.com>
2019-10-04 17:50:28 +0200

    Update evaluate.sh

--- engines/ontario/evaluate.sh
+++ engines/ontario/evaluate.sh
@@ -5,14 +5,14 @@ echo "size, query, run, type,time (date +%s.%N)" > ../results/results-times.csv
 
 for p in "${array[@]}"
 do
-        for i in 500
+        for i in 1 5 10 50 100 500 
         do
 			cp -r /data/gtfs-$p-$i/* /data/
 			# create config
 			/Ontario/scripts/create_rdfmts.py -s /configurations/datasources-$p.json -o /configurations/myconfig-$p.json
-			for j in 3
+			for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 			do
-				for t in 1
+				for t in 1 2 3 4 5
 				do
 					echo "++++++++++++++++++++++++++++++++++++++++++++++"
 					echo  "size-$i-q${j}.rq-run-$t-$p"
