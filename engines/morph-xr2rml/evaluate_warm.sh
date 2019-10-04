88bb4979c6f5603eaae584eb78cbfab9449468d2
Jhon Anibal <jhontb7@gmail.com>
2019-10-04 17:50:25 +0200

    Update evaluate_warm.sh

--- engines/morph-xr2rml/evaluate_warm.sh
+++ engines/morph-xr2rml/evaluate_warm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
+#size
 for i in 1 5 10 50 100 500
 do
-	for j in 11 14 17  #1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+	#queries
+	for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 	do
 		for t in 1 2 3 4 5
 		do
