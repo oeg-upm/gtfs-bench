7f651dfa158f63137dbcb9aef34d3140d0ac53a4
Jhon Anibal <jhontb7@gmail.com>
2019-10-04 15:58:22 +0200

    Update evaluate_warm.sh

--- engines/morph-xr2rml/evaluate_warm.sh
+++ engines/morph-xr2rml/evaluate_warm.sh
@@ -2,17 +2,27 @@
 
 for i in 1 5 10 50 100 500
 do
-	for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+	for j in 11 14 17  #1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 	do
 		for t in 1 2 3 4 5
 		do
 			# Load properties configuration
 			./pre_update_config.sh gtfs.morph-xr2rml.properties $i q${j}.rq 'warm'
 			# Run engine
-			timeout -s SIGKILL 60m  ./run.sh $i q${j}.rq $t 'warm' ||echo "$i, q${j}.rq, $t, warm, TimeOut">> ../results/results-times.csv
-			# Delete properties configuration
-			./post_update_config.sh gtfs.morph-xr2rml.properties
-
+			timeout -s SIGKILL 60m  ./run.sh $i q${j}.rq $t 'warm'
+			#Status of timeout
+			exit_status=$?
+			echo $exit_status
+			if [ $exit_status -eq 137 ]
+			then
+					./post_update_config.sh gtfs.morph-xr2rml.properties
+					echo "$i, q${j}.rq, $t, warm, TimeOut">> ../results/results-times.csv
+					echo "+++++++++++TimeOut: no more iterations+++++++++++++++"
+					break
+			else
+					./post_update_config.sh gtfs.morph-xr2rml.properties
+			fi
+			
 		done
 	done
 done
