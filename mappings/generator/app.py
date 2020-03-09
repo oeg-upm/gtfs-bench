f408984efb48493fc62e82986c959763d13789ce
Daniel Doña <daniel.dona@xnor.ga>
2020-03-09 13:25:03 +0100

    Config file format

--- mappings/generator/app.py
+++ mappings/generator/app.py
@@ -1,19 +1,12 @@
 import argparse
 from rdflib import Graph
 
-g = Graph()
-g.parse("../gtfs-csv.rml.ttl", format="ttl")
-
-
-
 parser = argparse.ArgumentParser(description='Generate multisource mapping file')
-parser.add_argmuent('--config-file', dest='in_config', required=True)
-parser.add_argument('--input-mapping', dest='in_mapping', required=True)
-parser.add_argument('--output-mapping', dest='out_mapping', required=True)
+#parser.add_argument('--config-file', dest='in_config', required=True)
+#parser.add_argument('--input-mapping', dest='in_mapping', required=True)
+#parser.add_argument('--output-mapping', dest='out_mapping', required=True)
 
 
 args = parser.parse_args()
 
-import pprint
-for stmt in g:
-    pprint.pprint(stmt)
+
