7598ace2dcc5b305b7554e589e08fcc229747b28
Daniel Doña <daniel.dona@xnor.ga>
2020-07-08 08:38:55 +0200

    Semantify

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 
 #--tag oegdataintegration/gtfs-bench:"$(git log --format="%h" -n 1)"
 docker build --build-arg versions_sensitive_commands="$(git log --format="%h" -n 1)" --tag oegdataintegration/gtfs-bench:latest .
-#docker push oegdataintegration/gtfs-bench:latest
+docker push oegdataintegration/gtfs-bench:latest
