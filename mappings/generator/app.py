47c384efaf95ef5c6d36aa84c99e2b36c9fc2d1d
Daniel Doña <daniel.dona@xnor.ga>
2020-09-21 19:32:31 +0200

    Fix XML mapping generation

--- mappings/generator/app.py
+++ mappings/generator/app.py
@@ -22,48 +22,49 @@ def dump_graph(g):
 class Source:
 	def __init__(self):
 		pass
-	
+
 class CSVSource(Source):
 	def __init__(self, filename):
 		self.filename = filename
 		self.graph = Graph()
-		
+
 	def get_graph(self, map_value):
 		self.graph.add((map_value, rml.logicalSource, ex.source))
 		self.graph.add((ex.source, rdf.type, rml.LogicalSource))
-		self.graph.add((ex.source, rml.source, Literal(self.filename, datatype=XSD.string)))	
-		self.graph.add((ex.source, rml.referenceFormulation, ql.CSV))	
-			
+		self.graph.add((ex.source, rml.source, Literal(self.filename, datatype=XSD.string)))
+		self.graph.add((ex.source, rml.referenceFormulation, ql.CSV))
+
 		return self.graph
-		
+
 class XMLSource(Source):
 	def __init__(self, filename):
 		self.filename = filename
 		self.graph = Graph()
-		
+
 	def get_graph(self, map_value):
 		self.graph.add((map_value, rml.logicalSource, ex.source))
 		self.graph.add((ex.source, rdf.type, rml.LogicalSource))
-		self.graph.add((ex.source, rml.source, Literal(self.filename, datatype=XSD.string)))	
-		self.graph.add((ex.source, rml.referenceFormulation, ql.CSV))	
-			
+		self.graph.add((ex.source, rml.source, Literal(self.filename, datatype=XSD.string)))
+		self.graph.add((ex.source, rml.referenceFormulation, ql.XPath))
+		self.graph.add((ex.source, rml.iterator, Literal("/Category/Record", datatype=XSD.string)));
+
 		return self.graph
-		
+
 class JSONSource(Source):
 	def __init__(self, filename):
 		self.filename = filename
 		self.graph = Graph()
-		
+
 	def get_graph(self, map_value):
 		self.graph.add((map_value, rml.logicalSource, ex.source))
 		self.graph.add((ex.source, rdf.type, rml.LogicalSource))
-		self.graph.add((ex.source, rml.source, Literal(self.filename, datatype=XSD.string)))	
+		self.graph.add((ex.source, rml.source, Literal(self.filename, datatype=XSD.string)))
 		self.graph.add((ex.source, rml.referenceFormulation, ql.JSONPath))
 		self.graph.add((ex.source, rml.iterator, Literal("$.[*]", datatype=XSD.string)))
-		
+
 		return self.graph
-		
-		
+
+
 class MySQLSource(Source):
 	def __init__(self, connection_uri, driver, user, password, table):
 		self.connection_uri = connection_uri
@@ -72,7 +73,7 @@ class MySQLSource(Source):
 		self.password = password
 		self.table = table
 		self.graph = Graph()
-		
+
 	def get_graph(self, map_value):
 		self.graph.add((map_value, rml.logicalSource, ex.source))
 		self.graph.add((ex.source, rdf.type, rml.LogicalSource))
@@ -94,7 +95,7 @@ class MongoSource(Source):
 		self.password = password
 		self.table = table
 		self.graph = Graph()
-		
+
 	def get_graph(self, map_value):
 		self.graph.add((map_value, rml.logicalSource, ex.source))
 		self.graph.add((ex.source, rdf.type, rml.LogicalSource))
@@ -113,74 +114,74 @@ class TriplesMap:
 		self.name = name
 		self.source = source
 		self.graph = Graph()
-		
+
 	def import_mapp(self, mapping_file):
 		self.graph.bind("rdf", rdf)
 		self.graph.parse(mapping_file, format="turtle")
-	
+
 	def uniquize(self):
-		
+
 		rainbow = {}
 
 		for subj, pred, obj in self.graph:
-				
+
 			if re.match("^http://mapping.example.com/.*", subj) and not re.match("^http://mapping.example.com/map_.*", subj):
 				if not (subj in rainbow):
-						
+
 					rainbow[subj] = URIRef("http://mapping.example.com/uuid/"+str(uuid.uuid4()))
 
 				self.graph.remove((subj,pred,obj))
 				self.graph.add((rainbow[subj], pred, obj))
 
 		for subj, pred, obj in self.graph:
-			
+
 			if re.match("^http://mapping.example.com/map.*", obj):
-				
+
 				pass
-			
+
 			elif re.match("^http://mapping.example.com/.*", obj):
 				if not (obj in rainbow):
-						
+
 					rainbow[obj] = URIRef("http://mapping.example.com/uuid/"+str(uuid.uuid4()))
 
 				self.graph.remove((subj,pred,obj))
 				self.graph.add((subj, pred, rainbow[obj]))
-				
-	
+
+
 	def link_source_graph(self):
-		
+
 		map_root = self.graph.value(subject=None, predicate = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), object = URIRef("http://www.w3.org/ns/r2rml#TriplesMap"), any=True)
 
 		if map_root is None:
-			
+
 			print("Ops!")
 
 		else:
 
 			self.graph += self.source.get_graph(map_root)
-		
-	
+
+
 	def get_graph(self):
-		
+
 		self.link_source_graph()
 		self.uniquize()
-			
+
 		return self.graph
 
 class Mapping:
-	
+
 	def __init__(self):
-		
+
 		self.graph = Graph()
-		
+
 	def process_config(self, config):
-		
+
 		self.triplesmaps = []
 
 		for e in config["entities"]:
-			
+
 			s = Source()
-			
+
 			if e["source"]["type"] == "mysql":
 				s = MySQLSource(e["source"]["connection"]["dsn"], e["source"]["connection"]["driver"], e["source"]["connection"]["user"], e["source"]["connection"]["pass"], e["source"]["table"])
 			elif e["source"]["type"] == "mongo":
@@ -191,30 +192,30 @@ class Mapping:
 				s = JSONSource(e["source"]["file"])
 			elif e["source"]["type"] == "xml":
 				s = XMLSource(e["source"]["file"])
-			
+
 			else:
 				print("Origen de datos " + str(e["source"]["type"]) + " no soportado!")
-			
+
 			ent = TriplesMap(e["name"], s)
-			
+
 			if "map" in e:
 				ent.import_mapp(e["map"])
-			
+
 			self.triplesmaps.append(ent)
-			
+
 			print("Processing TripleMap: " + e["name"])
 
 	def generate_graph(self):
-		
+
 		for ent in self.triplesmaps:
-			
+
 			self.graph += ent.get_graph()
-	
-				
+
+
 	def get_mapping(self):
-		
-		
-				
+
+
+
 		return self.graph
 
 parser = argparse.ArgumentParser(description='Generate multisource mapping file')
