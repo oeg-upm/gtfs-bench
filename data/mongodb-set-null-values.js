1293f8b475b715d424c3d1c345b4c3d40e5ce1d1
Daniel Doña <daniel.dona@xnor.ga>
2021-01-12 11:13:50 +0100

    Remove 'data' folder

--- data/mongodb-set-null-values.js
+++
@@ -1,7 +0,0 @@
-use gtfs
-db.AGENCY.updateMany({"agency_phone":""}, { $set: {"agency_phone":null} } )
-db.AGENCY.updateMany({"agency_fare_url":""}, { $set: {"agency_fare_url":null} } )
-db.ROUTES.updateMany({"route_desc":""}, { $set: {"route_desc":null} } )
-db.STOPS.updateMany({"stop_desc":""}, { $set: {"stop_desc":null} } )
-db.STOPS.updateMany({"stop_timezone":""}, { $set: {"stop_timezone":null} } )
-db.TRIPS.updateMany({"block_id":""}, { $set: {"block_id":null} } )
