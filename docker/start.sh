beaa870e2c2d9c37cf5fb0c63ad656a8ca9b3c42
Daniel Doña <daniel.dona@xnor.ga>
2021-01-25 17:55:56 +0100

    Revert changes removing mysql server by mistake

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,9 @@ mkdir /tmp/output/datasets/
 mkdir /tmp/output/mappings/
 mkdir /tmp/output/queries/
 
+echo "Starting auxiliar services..."
+service mysql start > /dev/null
+
 echo "Launching interface..."
 python3 composer/app.py
 
